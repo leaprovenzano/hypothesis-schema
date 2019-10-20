bb276e860dab85a401ba392b57561a410ea1e4a1
lea <leaprovenzano@gmail.com>
2019-10-20 21:09:58 +0100

    add string strategies

---
+++ src/hypothesis_schema/utils.py
@@ -0,0 +1,20 @@
+from typing import Callable
+import re
+from functools import lru_cache
+
+_pascal_exp = re.compile(r"([a-z\d])([A-Z])")
+
+
+@lru_cache(128)
+def to_snakecase(s: str) -> str:
+    return _pascal_exp.sub(r"\1_\2", s).lower()
+
+
+def accept_pascalcase(f: Callable):
+    """decorator for converting keyword args from pascalcase to snake case.
+    """
+
+    def inner(**kwargs):
+        return f(**{to_snakecase(k): v for k, v in kwargs.items()})
+
+    return inner
